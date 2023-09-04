501fe1ca0017b621ea0c9a3ae147b10e54485d26
Isoprene <143852270+Isoprene2@users.noreply.github.com>
2023-09-04 16:21:18 -0500

    Update 2023-08-21.js
    the filteringening begins

--- src/js/data/2023-08-21.js
+++ src/js/data/2023-08-21.js
@@ -1,15 +1,58 @@
-dataSetVersion = "2023-09-01"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2023-09-04"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
   {
-    name: "Filter out characters",
-      key: "erp",
-      tooltip: "Check this to restrict stuff",
-      checked: false,
+    name: "Filter by setting",
+      key: "setting",
+      tooltip: "Check these to only include characters from certain settings.",
+      checked: true,
       sub: [
-        { name: "Interrobang", key: "anti" },
-        { name: "NaNoWriMo", key: "nano" }
+        { name: "Lilaverse (Test Reality, There, Limbo)", key: "lila" },
+        { name: "SYOL", key: "syol" },
+        { name: "Asthenopia", key: "asth" },
+        { name: "RPG Hell", key: "rpg" },
+        { name: "Siderea", key: "side" },
+        { name: "Monster Defense Squad/Greater Underworld", key: "mds" },
+        { name: "Princess Soul Ultimatum", key: "psu" },
+        { name: "VN", key: "vn" },
+        { name: "Hell College", key: "hell" },
+        { name: "Heaven College", key: "heav" },
+        { name: "Celestella", key: "cele" },
+        { name: "Altirety/Interabang", key: "alt" },
+        { name: "En2rety", key: "en2" },
+        { name: "Al2rety", key: "al2" },
+        { name: "Misc.", key: "misc" }
+      ]
+    },
+  {
+    name: "Filter by group",
+      key: "group",
+      tooltip: "Check these to only include characters of certain categories.",
+      checked: true,
+      sub: [
+        { name: "Protagonists", key: "protag" },
+        { name: "Meme Characters", key: "meme" },
+        { name: "Superior Beings", key: "supb" },
+        { name: "Oress Holders", key: "oress" },
+        { name: "Them", key: "them" },
+        { name: "Representatives", key: "rep" },
+        { name: "Siderean Pantheon", key: "sidep" },
+        { name: "Fanfernals/Ignis-Adjacent", key: "ignis" },
+        { name: "Dicros", key: "dicro" }
+      ]
+    },
+  {
+    name: "Filter by notability",
+      key: "notab",
+      tooltip: "Check these to only include characters of certain levels of notability.",
+      checked: true,
+      sub: [
+        { name: "Popular OCs", key: "pop" },
+        { name: "Was in the Isonut Tournament", key: "tour" },
+        { name: "Has a Toyhouse profile", key: "th" },
+        { name: "Has been RPed with on Interrobang", key: "rp" },
+        { name: "Relevant to the En2rety plot", key: "plot" }
       ]
     }
   ];
@@ -19,112 +62,144 @@ dataSet[dataSetVersion].options = [
       name: "Aquari",
       img: "Aquari.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["pop", "tour", "rp", "plot"]
       }
     },
     {
       name: "Arachne",
       img: "Arachne.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp", "plot"]
       }
     },
     {
       name: "Arietis",
       img: "Arietis.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["pop", "tour", "rp", "plot"]
       }
     },
     {
       name: "Cancri",
       img: "Cancri.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     },
     {
       name: "Capricoris",
       img: "Capricoris.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     },
     {
       name: "Cetia",
       img: "Cetia.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["pop", "tour", "rp"]
       }
     },
     {
       name: "Geminorum",
       img: "Geminorum.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["pop", "tour", "rp", "plot"]
       }
     },
     {
       name: "Leonis",
       img: "Leonis.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["tour", "rp"]
       }
     },
     {
       name: "Librae",
       img: "Librae.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp", "plot"]
       }
     },
     {
       name: "Piscium",
       img: "Piscium.PNG",
       opts: {
-        game: ["nano"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["pop", "rp", "plot"]
       }
     },
     {
       name: "Sagittaria",
       img: "Sagittaria.PNG",
       opts: {
-        game: ["nano"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     },
     {
       name: "Scorpius",
       img: "Scorpius.PNG",
       opts: {
-        game: ["nano"]
+        setting: ["side"],
+        group: ["protag", "sidep"],
+        notab: ["pop", "tour", "rp", "plot"]
       }
     },
     {
       name: "Serpentarius",
       img: "Serpentarius.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     },
     {
       name: "Spes",
       img: "Spes.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["meme"],
+        notab: ["pop", "tour", "rp", "plot"]
       }
     },
     {
       name: "Tauri",
       img: "Tauri.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     },
     {
       name: "Virginis",
       img: "Virginis.PNG",
       opts: {
-        erp: [ "anti"]
+        setting: ["side"],
+        group: ["sidep"],
+        notab: ["rp"]
       }
     }
   ]
